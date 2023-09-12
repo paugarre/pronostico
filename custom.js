ade79115bfe154633d38bec17fe812848f828c83
tahirivazquez <tahiwdev@gmail.com>
2023-09-11 22:01:23 -0300

    Página funcionando
    Todo pronto

--- custom.js
+++ custom.js
@@ -1,85 +1,73 @@
-const result = document.querySelector('.result');
-const form = document.querySelector('.get-weather');
-const cityInput = document.querySelector('#city');
+const resultado = document.getElementById('resultado');
+const formulario = document.getElementById('obtenerClima');
+const inputCiudad = document.getElementById('ciudad');
 
-form.addEventListener('submit', (e) => {
+formulario.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    if (cityInput.value === '') {
-        showError('El campo de la ciudad es obligatorio...');
+    if (inputCiudad.value === '') {
+        mostrarError('El campo de la ciudad es obligatorio...');
         return;
     }
 
-    callAPI(cityInput.value);
-})
+    realizarLlamadaAPI(inputCiudad.value);
+});
 
-function callAPI(city){
-    const apiId = '41d1d7f5c2475b3a16167b30bc4f265c';
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiId}`;
+function realizarLlamadaAPI(ciudad) {
+    const claveAPI = '41d1d7f5c2475b3a16167b30bc4f265c';
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${claveAPI}`;
 
     fetch(url)
-        .then(data => {
-            return data.json();
+        .then(respuesta => {
+            return respuesta.json();
         })
-        .then(dataJSON => {
-            if (dataJSON.cod === '404') {
-                showError('Ciudad no encontrada...');
+        .then(datosJSON => {
+            if (datosJSON.cod === '404') {
+                mostrarError('Ciudad no encontrada...');
             } else {
-                clearHTML();
-                showWeather(dataJSON);
+                limpiarHTML();
+                mostrarClima(datosJSON);
             }
         })
         .catch(error => {
             console.log(error);
-        })
+        });
 }
 
-// Rest of your functions (showWeather, showError, kelvinToCentigrade, clearHTML) remain the same.
+function mostrarClima(datos) {
+    const { name, main: { temp, temp_min, temp_max }, weather: [arr] } = datos;
 
+    const grados = kelvinACentigrados(temp);
+    const min = kelvinACentigrados(temp_min);
+    const max = kelvinACentigrados(temp_max);
 
-
-
-function showWeather(data){
-    const {name, main:{temp, temp_min, temp_max}, weather:[arr]} = data;
-
-    const degrees = kelvinToCentigrade(temp);
-    const min = kelvinToCentigrade(temp_min);
-    const max = kelvinToCentigrade(temp_max);
-
-    const content = document.createElement('div');
-    content.innerHTML = `
+    const contenido = document.createElement('div');
+    contenido.innerHTML = `
         <h5>Clima en ${name}</h5>
-        <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icon">
-        <h2>${degrees}°C</h2>
-        <p>Max: ${max}°C</p>
-        <p>Min: ${min}°C</p>
+        <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icono">
+        <h2>${grados}°C</h2>
+        <p>Máx: ${max}°C</p>
+        <p>Mín: ${min}°C</p>
     `;
 
-    result.appendChild(content);
-
-    /* console.log(name);
-    console.log(temp);
-    console.log(temp_max);
-    console.log(temp_min);
-    console.log(arr.icon); */
+    resultado.appendChild(contenido);
 }
 
-function showError(message){
-    //console.log(message);
-    const alert = document.createElement('p');
-    alert.classList.add('alert-message');
-    alert.innerHTML = message;
+function mostrarError(mensaje) {
+    const alerta = document.createElement('p');
+    alerta.classList.add('alert-message');
+    alerta.innerHTML = mensaje;
 
-    form.appendChild(alert);
+    formulario.appendChild(alerta);
     setTimeout(() => {
-        alert.remove();
+        alerta.remove();
     }, 3000);
 }
 
-function kelvinToCentigrade(temp){
+function kelvinACentigrados(temp) {
     return parseInt(temp - 273.15);
 }
 
-function clearHTML(){
-    result.innerHTML = '';
+function limpiarHTML() {
+    resultado.innerHTML = '';
 }
